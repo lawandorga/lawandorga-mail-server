b900def64fe393f638f3195fd0e5e66c96c2d871
Einhard Leichtfuß <alguien@respiranto.de>
2023-01-13 10:31:15 +0100

    web/app/mail: Add plugin dovecot_impersonate_smtp
    This is a custom plugin; the previously set up dovecot_impersonate
    plugin does not handle SMTP authentication.
    
    Note that this plugin is licensed under GPL-2.0; this is due to being
    based upon and reliant ipon the equally licensed dovecot_impersonate
    plugin.

---
+++ ansible/roles/web/app/mail/files/plugins/dovecot_impersonate_smtp/main.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Amend dovecot_impersonate to also use the IMAP login name for SMTP.
+ *
+ * @author     Einhard Leichtfuß
+ * @copyright  2022 Einhard Leichtfuß
+ * @license    https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPL 2.0
+ */
+
+class dovecot_impersonate_smtp extends rcube_plugin
+{
+  public function init()
+  {
+    $this->add_hook('smtp_connect', array($this, 'impersonate_smtp'));
+  }
+
+
+  function impersonate_smtp($data)
+  {
+    if(isset($_SESSION['plugin.dovecot_impersonate_master']))
+    {
+      $data['smtp_user'] .= $_SESSION['plugin.dovecot_impersonate_master'];
+		}
+
+    return($data);
+	}
+}
