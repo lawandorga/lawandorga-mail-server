597c4382ab6256a41777d741fda8a9882debecb4
Einhard Leichtfuß <alguien@respiranto.de>
2024-03-12 04:15:29 +0100

    web/app/mail: Improve dovecot_impersonate_smtp pl.
    * Declare a public $task as recommended by the Wiki.
    * Add the public qualifier to functions to be explicit.
    * Use the new [] array syntax.
    
    This was induced by the previous changes to the login_page_extra_links
    plugin.

--- ansible/roles/web/app/mail/files/plugins/dovecot_impersonate_smtp/main.php
+++ ansible/roles/web/app/mail/files/plugins/dovecot_impersonate_smtp/main.php
@@ -4,27 +4,32 @@
  * Amend dovecot_impersonate to also use the IMAP login name for SMTP.
  *
  * @author     Einhard Leichtfuß
- * @copyright  2022, 2023 Einhard Leichtfuß
+ * @copyright  2022-2024 Einhard Leichtfuß
  * @license    https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html  GPL 2.0
  */
 
+// CONSIDER: Merge with `dovecot_impersonate` (i.e., fork it).
+//  - In that case, probably set $task = 'login|mail' (literally).
+
 class dovecot_impersonate_smtp extends rcube_plugin
 {
+  public $task = 'mail';
+
   private $rc;
 
 
   public function init()
   {
     $this->require_plugin('dovecot_impersonate');
-    $this->add_hook('smtp_connect', array($this, 'impersonate_smtp'));
+    $this->add_hook('smtp_connect', [$this, 'impersonate_smtp']);
 
     $this->rc = rcube::get_instance();
   }
 
 
-  function impersonate_smtp($data)
+  public function impersonate_smtp($data)
   {
-    if(isset($_SESSION['plugin.dovecot_impersonate_master']))
+    if (isset($_SESSION['plugin.dovecot_impersonate_master']))
     {
       // See
       // `/usr/share/roundcube/program/lib/Roundcube/rcube_smtp.php#connect()`.
@@ -36,6 +41,6 @@ class dovecot_impersonate_smtp extends rcube_plugin
       $data['smtp_user'] .= $_SESSION['plugin.dovecot_impersonate_master'];
     }
 
-    return($data);
+    return $data;
   }
 }
