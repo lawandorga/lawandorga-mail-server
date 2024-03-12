561332c876901c426b69579618bf69d17b166684
Einhard Leichtfuß <alguien@respiranto.de>
2024-03-12 04:14:38 +0100

    [bookworm] web/app/mail: Fix duplictd. extra links
    * We previously used rcube_plugin_api's add_content() to add the link.
      - Since Bookworm, this causes the links to be added twice.
      - This may be a bug in Roundcube.
      - This may have been a misuse of that method.
    * We now use a `template_container` hook instead, which--unlike
      add_content()--is documented in the Roundcube Wiki pages on writing
      plugins.
    
    Also
     - declare a public $task as recommended by the Wiki,
     - add the public qualifier to functions to be explicit,
     - use the new [] array syntax.

--- ansible/roles/web/app/mail/files/plugins/login_page_extra_links/main.php
+++ ansible/roles/web/app/mail/files/plugins/login_page_extra_links/main.php
@@ -4,35 +4,51 @@
  * Add extra links to the login page.
  *
  * @author     Einhard Leichtfuß
- * @copyright  2023 Einhard Leichtfuß
+ * @copyright  2023, 2024 Einhard Leichtfuß
  * @license    https://www.gnu.org/licenses/agpl-3.0.html  AGPL 3.0 or later
  */
 
 class login_page_extra_links extends rcube_plugin
 {
-  private $rc;
+  public $task = 'login';
 
-  function init()
+  private $rc;
+  private $extra_links_html;
+
+
+  public function init()
   {
     $this->rc = rcube::get_instance();
     $this->load_config();
 
-    $items = $this->rc->config->get('login_page_extra_links', array());
+    $items = $this->rc->config->get('login_page_extra_links', []);
 
     $sep = $this->rc->config->get
       ( 'login_page_extra_links_separator'
       , ' &nbsp;&bull;&nbsp;&nbsp;'
       );
 
+    $this->extra_links_html = '';
     foreach ($items as $item)
     {
-      $this->api->add_content
-        (   $sep
-          . '<a href="' . $item['url'] . '" target="_blank">'
-          . $item['label']
-          . '</a>'
-        , 'loginfooter'
-        );
+      $this->extra_links_html .=
+          $sep
+        . '<a href="' . $item['url'] . '" target="_blank">'
+        . $item['label']
+        . '</a>';
     }
+
+    $this->add_hook('template_container', [ $this, 'append_extra_links' ]);
+  }
+
+
+  public function append_extra_links($args)
+  {
+    if ($args['name'] == 'loginfooter')
+    {
+      return [ 'content' => $args['content'] . $this->extra_links_html ];
+    }
+
+    return null;
   }
 }
